ffcf86def0c586910bb36526a16036d789ed6773
Muhammad Wahyu Pratama <muhammad123wahyu123@gmail.com>
2026-06-08 13:20:42 +0700

    feat: make a feat navbar and responsive page

---
+++ readme.md
@@ -0,0 +1,19 @@
+# Password
+
+## Hasil1
+hasil 1 ini untuk layar diatas 768px
+
+![password](asset/hasil.png)
+
+## hasil2
+hasil 2 ketika layar di bawah 768px
+
+![password](asset/responsive1%20.png)
+
+## hasil 3
+hasil 3 ketika tombol icon garis 3 di klik pas di ukuran layar 768px
+
+![password](asset/responsiv2.png)
+
+
+
--- script.js
+++ script.js
@@ -1,13 +1,45 @@
 const password = document.getElementById("password");
-const button = document.querySelector("button");
-const img = document.querySelector(".pass img");
+const confirmPassword = document.getElementById("confirm-password");
+const eyeButton1 = document.querySelector(".eye-btn");
+const eyeButton2 = document.querySelector(".eye-btn2");
+const img1 = document.querySelector(".eye-btn img");
+const img2 = document.querySelector(".eye-btn2 img");
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
 
-button.addEventListener("click", () => {
-    if (password.type === "password") {
+eyeButton1.addEventListener("click", () => {
+
+    if(password.type === "password"){
+
         password.setAttribute("type", "text");
-        img.setAttribute("src", "asset/eye-closed-thin.svg");
-    } else {
+        img1.setAttribute("src", "asset/eye-closed-thin.svg");
+
+    }else{
+
         password.setAttribute("type", "password");
-        img.setAttribute("src", "asset/eye.svg");
+        img1.setAttribute("src", "asset/eye.svg");
+
     }
+
+});
+
+eyeButton2.addEventListener("click", () => {
+
+    if(confirmPassword.type === "password"){
+
+        confirmPassword.setAttribute("type", "text");
+        img2.setAttribute("src", "asset/eye-closed-thin.svg");
+
+    }else{
+
+        confirmPassword.setAttribute("type", "password");
+        img2.setAttribute("src", "asset/eye.svg");
+
+    }
+
+});
+
+hamburger.addEventListener("click", () => {
+    console.log("hamburger diklik");
+    navLinks.classList.toggle("show");
 });
